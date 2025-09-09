373d51069061f7ec1cf6324bb14e53e279253b36
saskiana-06 <rsaskiana@gmail.com>
2025-09-09 18:08:12 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,19 @@
+public class Saskiana_D0225005 {
+    public static void main(String[] args) {
+penggunaan println
+        System.out.println("Nama: Saskiana");
+        System.out.println("Umur: 18 tahun");
+        System.out.println("Kelas: Informatika A");
+        
+penggunaan print
+        System.out.print("Hallo!! nama saya Saskiana nama familiar kampus adalah kia, ");
+        System.out.print("Umur saya 18 tahun dari kelas A Informatika");
+        
+Penggunaan printf
+        System.out.printf("%nNama:%s %nUmur:%d %nProdi: %s %nBerat badan: %.1f", "SASKIANA",18,"INFORMATIKA",55.5);
+        System.out.println();
+        
+    }
+    
+    
+}
