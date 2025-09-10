9202e73e52a77b080e7f74cf89c9376b28533ee3
saskiana-06 <rsaskiana@gmail.com>
2025-09-10 15:36:56 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,17 @@
+public class Day3 {
+    public static void main(String[] args) {
+//  Penggunaan Println untuk membuat baris baru
+        System.out.println("Nama : Saskiana");
+        System.out.println("Ibu  : Supriani");
+        System.out.println("Ayah : Ramli");
+        System.out.println("Alamat : Batupapan,Desa Papalang");
+        
+//  Penggunaan Print agar hasil yang kita cetak tidak berpindah ke baris baru atau tetap bersambung. 
+        System.out.print("Haii!! nama saya Saskiana nama ibu saya Supriani dan nama ayah saya Ramli.");
+        System.out.print(" Alamat saya Batupapan, Desa Papalang. ");
+        
+//  Penggunaan Printf digunakan untuk menampilkan teks dengan format tertentu, misalnya angka, bilangan desimal, atau menampilkan variabel.
+        System.out.printf("%nNama :%s %nAngkatan :%d %nProdi :%s %nNilai Ujian :%.1f", "Saskiana",2025,"INFORMATIKA", 99.9);
+        
+    }
+}
