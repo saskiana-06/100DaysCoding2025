4cbd0a356efd74d0112ea1a692c27206f4ecbfc9
saskiana-06 <rsaskiana@gmail.com>
2025-09-11 12:24:14 +0700

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,12 @@
+public class Day4 {
+    public static void main(String[] args) {
+        String nama,alamat;
+        nama = "Saskiana";
+        alamat = "Batupapan";
+        int umur = 18;
+        System.out.println("\nNama saya\t\t           : " + nama);
+        System.out.println("\nsaya berasal dari\t\t : " + alamat);
+        System.out.println("\nUmur saya saat ini\t\t : + umur");
+    }
+   
+}
