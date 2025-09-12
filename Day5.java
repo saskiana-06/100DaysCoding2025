baff8bd3339bf4bfca58f72bd4a7e6d1326130f5
saskiana-06 <rsaskiana@gmail.com>
2025-09-12 18:14:41 +0700

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,14 @@
+public class Day5 {
+    public static void main(String[] args) {
+        byte angka1 = 126;
+        short angka2 = 32600;
+        int angka3 = 670030509;
+        long angka4 = 2000000000l;
+        System.out.println("Tipe data byte: "+angka1);
+        System.out.println("Tipe data short: "+angka2);
+        System.out.println("Tipe data integer: "+angka3);
+        System.out.println("Tipe data long: "+angka4);
+
+
+    }
+}
