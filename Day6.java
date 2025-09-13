35d500c48fca648298e038515a2e0452dc3d62ef
saskiana-06 <rsaskiana@gmail.com>
2025-09-13 10:42:46 +0700

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,10 @@
+public class Day6 {
+    public static void main(String[] args) {
+        
+        double tinggi = 155.5;
+        float km = 2.4f;
+        System.out.println("Tinggi saya: "+tinggi);
+        System.out.println("Jarak kost ke kampus: "+km+" kilometer");
+    
+    }
+}
