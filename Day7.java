c830cd07d898e01f3aecd4b7d0582bcf5cc31629
saskiana-06 <rsaskiana@gmail.com>
2025-09-14 20:49:33 +0800

    Day7.java

--- Day7.java
+++ Day7.java
@@ -1,9 +1,8 @@
-public class Day07 {
+public class Day7 {
     public static void main(String[] args) {
-     char inisial = 'S';
-     boolean no = true;
-        System.out.println("Inisial nama saya adalah "+inisial+"(Saskiana)");
-        System.out.println(no);
+        char inisial = 'S';
+        boolean name = true;
+        System.out.println("Inisial nama saya adalah"+" "+inisial+" "+"(Saskiana)");
+        System.out.println(name);
     }
-  
 }
