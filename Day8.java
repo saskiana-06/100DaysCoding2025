69d77d5895d38277b49e34506a573808bf75d421
saskiana-06 <rsaskiana@gmail.com>
2025-09-15 18:35:13 +0700

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,14 @@
+public class Day8 {
+    public static void main(String[] args) {
+        String nama = "Saskiana";
+        String nim = "D0225005";
+        String kelas = "Informatika A";
+        String alamat = "Batupapan";
+        System.out.println("Nama\t: "+nama);
+        System.out.println("NIM\t: "+nim);
+        System.out.println("Kelas\t: "+kelas);
+        System.out.println("Alamat\t: "+alamat);
+        
+    // String digunakan untuk teks maupun angka     
+    }
+}
