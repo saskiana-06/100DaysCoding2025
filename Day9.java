d0cd1e442052bc5892ae7a7da2c4c6853eb99d66
saskiana-06 <rsaskiana@gmail.com>
2025-09-16 18:13:36 +0700

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,36 @@
+public class Day9 {
+    public static void main(String[] args) {
+    //Deklarasi
+        String nama,ayah,saudara,alamat,alumni;
+        int umur,jumlah;
+        double jarak;
+        
+    //Inisialisasi
+        nama = "Saskiana";
+        ayah = "Ramli";
+        saudara = "Muh. Naufal Ramli";
+        alamat = "Batupapan";
+        alumni = "UPTD SMKN PAPALANG";
+        umur = 18;
+        jumlah = 2;
+        jarak = 1.9;
+    
+    //Update Variabel
+        nama = "Supriani";
+        ayah = "Sumpeno";
+        saudara = "Sri dan Suntoro";
+        alamat = "Batupapan";
+        alumni = "SMA PGRI";
+        umur = 35;
+        jumlah = 3;
+        jarak = 198.5;
+        System.out.println("Nama saya:"+nama);
+        System.out.println("Nama ayah saya:"+ayah);
+        System.out.println("Nama saudara saya:"+saudara);
+        System.out.println("Alamat saya:"+alamat);
+        System.out.println("Saya Alumni dari:"+alumni);
+        System.out.println("Umur saya:"+umur);
+        System.out.println("Jumlah Saudara saya:"+jumlah);
+        System.out.println("Jarak rumah ke kost:"+jarak + "km");
+    }
+}
