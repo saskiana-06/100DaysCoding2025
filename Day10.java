afd50c8364f2ff66cbe15062236eaf5346bfc703
saskiana-06 <rsaskiana@gmail.com>
2025-09-17 20:39:49 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,12 @@
+public class Day10 {
+    public static void main(String[] args) {
+        final String brother = "Naufal";
+        final byte umur = 10;
+        final int tinggi = 140;
+        final double bb = 50.5;
+        System.out.println("Nama brother:"+ brother);
+        System.out.println("Umur adik:"+ umur + "thn");
+        System.out.println("Tinggi badan:"+ tinggi);
+        System.out.println("Berat badan:"+ bb);
+    }
+}
