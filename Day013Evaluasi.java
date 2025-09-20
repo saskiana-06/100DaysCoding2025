8acaadfa5c9eaa77ef1bb0ad314dfeb3e415388d
saskiana-06 <rsaskiana@gmail.com>
2025-09-20 19:28:22 +0700

    Day013Evaluasi.java

---
+++ Day013Evaluasi.java
@@ -0,0 +1,94 @@
+public class Evaluasi1 {
+    public static void main(String[] args) {
+        String nama,alamat;
+        
+     
+        //membuat 9 tipe data memiliki 2 variabel
+        int umur = 2;
+        int umur2 = 3;
+        long panjang = 12345;
+        long panjang2 = 67905;
+        byte luas = 23;
+        byte luas2 = 22;
+        short pendek = 789;
+        short pendek2 = 7789;
+        double koma = 2.9;
+        double koma2 = 6.9;
+        float yah = 28.9f;
+        float yah2 = 27.8f;
+        char subjek = 's';
+        char subjek2 = 'p';
+        String name = "saskiana";
+        String nama2 = "batupapan";
+        boolean benar = true;
+        boolean salah = false;
+        
+        
+        System.out.println("Sebelum update");
+        System.out.println(luas2);
+        System.out.println(luas);
+        System.out.println(pendek);
+        System.out.println(pendek2);
+        System.out.println(panjang);
+        System.out.println(panjang2);
+        System.out.println(umur);
+        System.out.println(umur2);
+        System.out.println(koma);
+        System.out.println(koma2);
+        System.out.println(yah);
+        System.out.println(yah2);
+        System.out.println(subjek);
+        System.out.println(subjek2);
+        System.out.println(name);
+        System.out.println(nama2);
+        System.out.println(benar);
+        System.out.println(salah);
+        System.out.println();
+        
+     
+        
+        umur = 12;
+        umur2 = 31;
+        panjang = 1235;
+        panjang2 = 167905;
+        luas = 27;
+        luas2 = 92;
+        pendek = 7879;
+        pendek2 = 7989;
+        koma = 8.9;
+        koma2 = 7.9;
+        yah = 28.9f;
+        yah2 = 67.8f;
+        subjek = 'a';
+        subjek2 = 'k';
+        name = "padli";
+        nama2 = "naufal";
+        
+        
+        
+        //cetak dengan print,println,printf
+        System.out.println("Sesudah update");
+        System.out.println(luas2);
+        System.out.println(luas);
+        System.out.println(pendek);
+        System.out.println(pendek2);
+        System.out.println(panjang);
+        System.out.println(panjang2);
+        System.out.println(umur);
+        System.out.println(umur2);
+        System.out.println(koma);
+        System.out.println(koma2);
+        System.out.println(yah);
+        System.out.println(yah2);
+        System.out.println(subjek);
+        System.out.println(subjek2);
+        System.out.printf(("%s"),name);
+        System.out.printf(("\n%s"),nama2);
+        System.out.print("\n"+benar);
+        System.out.print("\n"+salah);
+        
+        
+        
+        
+    }
+}
