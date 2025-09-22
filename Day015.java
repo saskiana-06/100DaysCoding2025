f721f41e08fcd90a7c6c940e1f1ac245b908c7c8
saskiana-06 <rsaskiana@gmail.com>
2025-09-22 20:36:42 +0700

    Day015.java

--- Day015.java
+++ Day015.java
@@ -1,17 +1,17 @@
 public class Day15 {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
-        System.out.println("Masukkan angka 8 :");
+        System.out.print("Masukkan angka 8 :");
         int a = input.nextInt();
-        System.out.println("Masukkan angka 6 :");
+        System.out.print("Masukkan angka 6 :");
         int b = input.nextInt();
         
         
         int Penjumlahan = a + b;
-        System.out.println("Hasil penjumlahan" + Penjumlahan);
+        System.out.println("Hasil penjumlahan\t" + Penjumlahan);
       
         int Pengurangan = a - b;
-        System.out.println("Hasil pengurangan" + Pengurangan);
+        System.out.println("Hasil pengurangan\t" + Pengurangan);
        
     }
    
