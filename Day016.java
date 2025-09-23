8b71ddac97a78253ecfd09e4ada4c818448d9075
saskiana-06 <rsaskiana@gmail.com>
2025-09-23 18:48:02 +0700

    Day016.java

--- Day016.java
+++ Day016.java
@@ -1,19 +1,22 @@
 public class Day16 {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
-        System.out.print("Masukkan angka 6 :");
-        int caca = input.nextInt();
-        System.out.print("Masukkan angka 2 :");
-        int didi = input.nextInt();
+        System.out.print("Masukkan angka pertama :");
+        double angka1 = input.nextDouble();
+        System.out.print("Masukkan angka kedua :");
+         double angka2 = input.nextDouble();
         
         
-        int Perkalian = caca * didi;
-        System.out.println("Hasil perkalian\t" + Perkalian);
+        double Hasil_Perkalian = angka1 * angka2;
+        System.out.println("Hasil perkalian\t" + Hasil_Perkalian);
       
-        int Pembagian = caca / didi;
-        System.out.println("Hasil pembagian\t" + Pembagian);
+        double Hasil_Pembagian = angka1 / angka2;
+        System.out.println("Hasil pembagian\t" + Hasil_Pembagian);
        
     }
-  
+ 
+        
+        
+    
   
 }
