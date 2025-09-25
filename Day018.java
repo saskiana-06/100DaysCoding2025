0b3d4fd972c087b690729ef64573fbc9c70ed6ec
saskiana-06 <rsaskiana@gmail.com>
2025-09-25 17:53:28 +0700

    Day018.java

---
+++ Day018.java
@@ -0,0 +1,8 @@
+public class Day18 {
+    public static void main(String[] args) {
+        byte nol = 120; 
+        short kecil = nol;
+        System.out.println("Konversi dari byte ke short:"+nol);
+        
+    }
+}
