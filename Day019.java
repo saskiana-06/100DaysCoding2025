fb3e499dd5e9f21880bd060f3a9066368be6ef5c
saskiana-06 <rsaskiana@gmail.com>
2025-09-26 21:08:37 +0700

    Day019.java

---
+++ Day019.java
@@ -0,0 +1,8 @@
+public class NewClass {
+    public static void main(String[] args) {      
+    int besar = 1976467654;
+    byte tinggi = (byte)besar;
+            System.out.println("Konfersi long ke int :"+besar); 
+            
+            }
+}
