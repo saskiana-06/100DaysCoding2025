415527b249413545e8388e86c49927f3c4f0cdce
saskiana-06 <rsaskiana@gmail.com>
2025-09-27 20:11:53 +0700

    Day020.java

---
+++ Day020.java
@@ -0,0 +1,12 @@
+public class Day20 {
+    public static void main(String[] args) {   
+    long panjang = 1283183298;
+    String huruf = Long.toString(panjang);
+        System.out.println("mengubah primitif ke string :"+huruf);
+    char kamu ='K';
+    String dia = Character.toString(kamu);
+        System.out.println("mengubah primitif ke String pke char :"+dia);
+       
+
+    }
+}
