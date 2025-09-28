37a73bdc4198217012b66e8d0ec07ef1c7e074d8
saskiana-06 <rsaskiana@gmail.com>
2025-09-28 14:17:57 +0700

    Day021.java

---
+++ Day021.java
@@ -0,0 +1,16 @@
+package D0225005;
+public class Day21 {
+    public static void main(String[] args) { 
+        
+       int a = 12;
+       int b = 14;
+       a =(a+b); 
+       b = (a-b);
+       a = (a-b);
+      
+        System.out.println(a);
+        System.out.println(b);
+        
+        
+        }       
+    }   
