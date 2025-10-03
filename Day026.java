5c6b66bfa7cd3674488fa2b71c6a1b3e4bc8c049
saskiana-06 <rsaskiana@gmail.com>
2025-10-03 20:46:24 +0700

    Day026.java

--- Day026.java
+++ Day026.java
@@ -1,3 +1,4 @@
+
 package D0225005;
 
 import java.util.Scanner;
@@ -7,15 +8,25 @@ public class Day26 {
         Scanner in = new Scanner (System.in);
         System.out.print("Masukkan angka: ");       
         int angka = in.nextInt();
-        System.out.print("massukkan angka kedua: ");
+        System.out.print("masukkan angka kedua: ");
         int angka2 = in.nextInt();
+        System.out.print("masukkan angka ketiga: ");
+        int angka3 = in.nextInt();
+        System.out.print("masukkan angka keempat: ");
+        int angka4 = in.nextInt();
         
-        angka += 4;
-        angka2 *=6;
+        angka += 2;
+        angka2 -= 2;
+        angka3 *= 2;
+        angka4 /= 2;
+        
        
         System.out.println(angka);
         System.out.println(angka2);
-        
+        System.out.println(angka3);
+        System.out.println(angka4);
+       
     }
   
 }
+  
