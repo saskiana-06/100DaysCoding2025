8f1e399073c351b366f5b5f208991e215b45acb8
saskiana-06 <rsaskiana@gmail.com>
2025-10-07 19:42:56 +0700

    Day030.java

---
+++ Day030.java
@@ -0,0 +1,21 @@
+package Day1Coding;
+
+import java.util.Scanner;
+
+public class Day28 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.print("masukkan angka: ");
+        int a = in.nextInt();
+        System.out.print("masukkan angka: ");
+        int b = in.nextInt();
+        
+        System.out.println("Apakah nilai a lebih kecil sama dengan b? : " + (a<=b));
+        System.out.println("Apakah nilai a lebih besar sama dengan b? : " + (a>=b));
+        
+
+
+
+    }
+  
+}
