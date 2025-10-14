d83a224eb31b24d9806c14b432bcdd5cee745606
saskiana-06 <rsaskiana@gmail.com>
2025-10-14 19:21:05 +0700

    Day037.java

---
+++ Day037.java
@@ -0,0 +1,17 @@
+package D0225005; 
+
+import java.util.Scanner;
+
+public class Day37 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.print("masukkan nilai : ");
+        int nilai = in.nextInt();
+        if (angka%2==0){
+            System.out.println("Angka Genap ");
+        }else{
+            System.out.println("Angka Ganjil ");
+        }
+        
+    }
+}
