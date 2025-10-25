1368d526674292b8aee25428c1de4b82dd616889
saskiana-06 <rsaskiana@gmail.com>
2025-10-25 20:00:45 +0700

    Day048.java

---
+++ Day048.java
@@ -0,0 +1,47 @@
+package D0225005;
+
+import java.util.Scanner;
+
+public class Day48 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double angka1, angka2, hasil;
+        char operator;
+
+        System.out.print("Masukkan angka pertama: ");
+        angka1 = input.nextDouble();
+
+        System.out.print("Masukkan operator (+, -, *, /): ");
+        operator = input.next().charAt(0);
+
+        System.out.print("Masukkan angka kedua: ");
+        angka2 = input.nextDouble();
+
+        switch (operator) {
+            case '+':
+                hasil = angka1 + angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+            case '-':
+                hasil = angka1 - angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+            case '*':
+                hasil = angka1 * angka2;
+                System.out.println("Hasil: " + hasil);
+                break;
+            case '/':
+                if (angka2 != 0) {
+                    hasil = angka1 / angka2;
+                    System.out.println("Hasil: " + hasil);
+                } else {
+                    System.out.println("Error: Kamu tidak bisa membaginya dengan nol sama seperti cintaku!");
+                }
+                break;
+            default:
+                System.out.println("Operator tidak valid!");
+        }
+
+        
+    }
+}
