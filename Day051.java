34af9f8dc37e5a520cea4fde49d9b5572a8ffbb0
saskiana-06 <rsaskiana@gmail.com>
2025-10-28 19:01:03 +0700

    Day051.java

---
+++ Day051.java
@@ -0,0 +1,12 @@
+package D0225005;
+import java.util.Scanner;
+public class Day51 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan angka :");
+        int angka = in.nextInt();
+        for (int i = 0; i <= angka; i++) {
+            System.out.println(i);
+        }
+           
+    }
