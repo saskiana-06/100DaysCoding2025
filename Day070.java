d33d74c6b025baf796a556632d11e4f1ef21c981
saskiana-06 <rsaskiana@gmail.com>
2025-11-16 18:37:53 +0700

    Day070.java

---
+++ Day070.java
@@ -0,0 +1,17 @@
+public class Day70 {
+    public static void main(String[] args) {
+      int per = 7;
+      int per2 = 4;
+      
+        for (int i = 1; i <=per2; i++) {
+            for (int j = 1; j <= per; j++) {
+             System.out.print(" * ");     
+            }
+            System.out.println();     
+            }
+             
+                
+            }
+          
+      
+        }
