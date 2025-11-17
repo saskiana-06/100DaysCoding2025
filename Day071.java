ae41b68ed7c8776bbfd9184179c4d1e64285dba2
saskiana-06 <rsaskiana@gmail.com>
2025-11-17 21:00:08 +0700

    Day071.java

---
+++ Day071.java
@@ -0,0 +1,12 @@
+public class Day071.java {
+    public static void main(String[] args) {
+        int N = 5;
+        
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("6");  
+            }
+            System.out.println();
+        }
+        }  
+    }
