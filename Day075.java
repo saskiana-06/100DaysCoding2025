94294ad0c469a584b39ca41dfd9f746a21d7c805
saskiana-06 <rsaskiana@gmail.com>
2025-11-21 20:19:27 +0800

    Day075.java

---
+++ Day075.java
@@ -0,0 +1,10 @@
+public class Day75 {
+    public static void main(String[] args) {
+        String kalimat = "Halo Guys";
+
+        System.out.println("Teks : " + kalimat);
+        System.out.println("Panjang Teks : " + kalimat.length());
+        System.out.println("Uppercase : " + kalimat.toUpperCase());
+        System.out.println("Lowercase : " + kalimat.toLowerCase());
+    }
+}
