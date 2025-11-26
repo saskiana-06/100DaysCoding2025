ceacba13dee87cd87b06ed290f823b408c5d4cdd
saskiana-06 <rsaskiana@gmail.com>
2025-11-26 19:13:17 +0700

    Day080.java

---
+++ Day080.java
@@ -0,0 +1,8 @@
+public class Day80 {
+
+    public static void main(String[] args) {
+        int nilai [] = {90, 80, 75};
+        System.out.println("=== Daftar nilai siswa ===");
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println(nilai[i]);  
+        }
