d6922f382f23760fffeb72a5f8ab2250debc3eb7
saskiana-06 <rsaskiana@gmail.com>
2025-12-01 21:45:53 +0700

    Day085.java

---
+++ Day085.java
@@ -0,0 +1,17 @@
+public class Day85 {
+    public static void main(String[] args) {
+          int[] angka = {5, 2, 9, 1, 7};
+
+        int max = angka[0]; // ambil angka pertama dulu
+
+        for (int i = 1; i < angka.length; i++) {
+            if (angka[i] > max) { 
+                max = angka[i];  // kalau lebih besar, ganti jadi max baru
+            }
+        }
+
+        System.out.println("Angka terbesar/Maximal : " + max);
+
+    }
+ 
+}
