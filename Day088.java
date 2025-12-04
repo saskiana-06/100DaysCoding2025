f2b6b13d5dd21e26534f788c39593c985b431c0a
saskiana-06 <rsaskiana@gmail.com>
2025-12-04 17:21:25 +0700

    Day088.java

---
+++ Day088.java
@@ -0,0 +1,12 @@
+public class Day88 {
+    public static void main(String[] args) {
+        int[] nilai = {2, 4, 6, 8};
+        int total = 0;
+
+        for (int n : nilai) {
+            total += n;    
+        }
+
+        System.out.println("Total: " + total);
+    }
+}
