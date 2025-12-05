4f08f3183c39dbf4f49ca7242838c8193eab3f19
saskiana-06 <rsaskiana@gmail.com>
2025-12-05 21:19:46 +0800

    Day089.java

---
+++ Day089.java
@@ -0,0 +1,12 @@
+public class ContohVoid {
+    
+    // Method void
+    void sapa() {
+        System.out.println("Dunia tipu-tipu");
+    }
+
+    public static void main(String[] args) {
+        ContohVoid obj = new ContohVoid();
+        obj.sapa();
+    }
+}
