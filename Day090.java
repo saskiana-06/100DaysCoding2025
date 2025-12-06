1727aeba0d1a21fdf549057bfb0b4cb970f1bbfc
saskiana-06 <rsaskiana@gmail.com>
2025-12-06 20:11:37 +0700

    Day090.java

---
+++ Day090.java
@@ -0,0 +1,13 @@
+public class Day85 {
+
+    public int umur(int umur) {
+        return umur;
+    }
+
+    public static void main(String[] args) {
+        Day85 c = new Day85();
+        c.umur(18);
+        System.out.println(c.umur(18) + " tahun");
+
+    }
+}
