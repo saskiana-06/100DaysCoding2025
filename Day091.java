b5a3729732d65c481475937d8929e29798874222
saskiana-06 <rsaskiana@gmail.com>
2025-12-07 20:34:15 +0700

    Day091.java

---
+++ Day091.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    static double luas(double alas, double tinggi) {
+        double luas = 0.5 * alas * tinggi;
+        return luas;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(luas(2,4));
+    }
+
+}
