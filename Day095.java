5694c92603b5b66f4dd4b60b45637d1cdf8fa1a9
saskiana-06 <rsaskiana@gmail.com>
2025-12-11 21:13:04 +0700

    Day095.java

---
+++ Day095.java
@@ -0,0 +1,10 @@
+public class Day95 {
+
+    public static void cetakPesan(String pesan) {
+        System.out.println("Pesan: " + pesan);
+    }
+
+    public static void main(String[] args) {
+        cetakPesan("Uas Mtk Gacorrr");
+    }
+}
